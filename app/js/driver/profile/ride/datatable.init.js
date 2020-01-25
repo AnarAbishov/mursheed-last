5a31ddfa8ab073c94129efc858ba552415b53377
parvizrovshanaliyev <parvizra@code.edu.az>
2020-01-25 18:06:37 +0400

    row details  js ready

--- app/js/driver/profile/ride/datatable.init.js
+++ app/js/driver/profile/ride/datatable.init.js
@@ -96,30 +96,80 @@ rideDataTable.on('draw',
 //#endregion
 
 function format(row) {
-    // variables
-    var card =
-        `<tr style="width: 100%">
-            <td>Baku</td>
-            <td>1</td>
-            <td>Ganja</td>
-            <td>3</td>
-            <td>Baku-Ganja</td>
-            <td>12</td>
-            <td>$20</td>
-            <td>That was good.</td>
-        </tr>`,
-        // loop item
-        cardEnd = `   </div>
-                    </div>
-                </div>`;
-    //
-    card += `<div class="col-lg-4">
-        <div class="card e-co-product">
-            <a href="">
-                <img id="" src="" alt="" class="img-fluid">
-            </a>
-        </div><!--end card-->
-    </div><!--end col-->`;
+
+    var card = `<div class="card">
+                 <div class="card-body">
+                    <h4 class="header-title alert-info header-title p-2">Routes</h4>
+                     <div class="single-table">
+                         <div class="table-responsive">`,
+
+        cardEnd = `</div>
+                        </div>
+                     </div>
+                 </div>`,
+        // route table
+        routeTable = `<table class="table table-hover text-center">`,
+        routeTableEnd = `</table>`;
+        
+        
+    //#region table
+
+    // table first row
+    routeTable += `<thead >
+                          <tr>
+                              <th scope="col">From City</th>
+                              <th scope="col">To City</th>
+                              <th scope="col">Price</th>
+                              <th scope="col">Info</th>
+                          </tr>
+                   </thead>`;
+
+    Array.prototype.forEach.call(row.routes,
+        function (item) {
+
+            routeTable += `<tbody>
+                                <tr>
+                                    <th scope="row">${item.fromCity}</th>
+                                    <td class="class="alert-link"">${item.toCity}</td>
+                                    <td>${item.price}</td>
+                                    <td class="alert alert-primary">${item.info}</td>
+                                </tr>
+                            </tbody>`;
+
+        });
+    routeTable += routeTableEnd;
+    //#endregion DocDirection and DocDirectionDetails
+    card += routeTable;
     card += cardEnd;
     return card;
 }
+//#endregion
+
+// function format(row) {
+//     // variables
+//     var card =
+//         `<tr style="width: 100%">
+//             <td>Baku</td>
+//             <td>1</td>
+//             <td>Ganja</td>
+//             <td>3</td>
+//             <td>Baku-Ganja</td>
+//             <td>12</td>
+//             <td>$20</td>
+//             <td>That was good.</td>
+//         </tr>`,
+//         // loop item
+//         cardEnd = `   </div>
+//                     </div>
+//                 </div>`;
+//     //
+//     card += `<div class="col-lg-4">
+//         <div class="card e-co-product">
+//             <a href="">
+//                 <img id="" src="" alt="" class="img-fluid">
+//             </a>
+//         </div><!--end card-->
+//     </div><!--end col-->`;
+//     card += cardEnd;
+//     return card;
+// }
