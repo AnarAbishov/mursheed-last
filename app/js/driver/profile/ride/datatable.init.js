9757f22a99b1e3ed8a4f6ad052cd8c0887ab2162
parvizrovshanaliyev <parvizra@code.edu.az>
2020-01-25 17:58:03 +0400

    ride routes loop loading

--- app/js/driver/profile/ride/datatable.init.js
+++ app/js/driver/profile/ride/datatable.init.js
@@ -94,3 +94,32 @@ rideDataTable.on('draw',
             });
     });
 //#endregion
+
+function format(row) {
+    // variables
+    var card =
+        `<tr style="width: 100%">
+            <td>Baku</td>
+            <td>1</td>
+            <td>Ganja</td>
+            <td>3</td>
+            <td>Baku-Ganja</td>
+            <td>12</td>
+            <td>$20</td>
+            <td>That was good.</td>
+        </tr>`,
+        // loop item
+        cardEnd = `   </div>
+                    </div>
+                </div>`;
+    //
+    card += `<div class="col-lg-4">
+        <div class="card e-co-product">
+            <a href="">
+                <img id="" src="" alt="" class="img-fluid">
+            </a>
+        </div><!--end card-->
+    </div><!--end col-->`;
+    card += cardEnd;
+    return card;
+}
--- app/js/profile/profileTourist.js
+++ app/js/profile/profileTourist.js
@@ -67,31 +67,3 @@ function readSelectData(row) {
     return selectStart;
 }
 
-function format(row) {
-    // variables
-    var tr =
-        `<tr style="width: 100%">
-            <td>Baku</td>
-            <td>1</td>
-            <td>Ganja</td>
-            <td>3</td>
-            <td>Baku-Ganja</td>
-            <td>12</td>
-            <td>$20</td>
-            <td>That was good.</td>
-        </tr>`,
-        // loop item
-        cardEnd = `   </div>
-                    </div>
-                </div>`;
-    //
-    card += `<div class="col-lg-4">
-        <div class="card e-co-product">
-            <a href="">
-                <img id="" src="" alt="" class="img-fluid">
-            </a>
-        </div><!--end card-->
-    </div><!--end col-->`;
-    card += cardEnd;
-    return card;
-}
