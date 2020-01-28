e3bd71d524ee5ba70de2f15313b862ff7648bea0
Kamran Mmmdv <amorfrap@gmail.com>
2020-01-28 14:23:26 +0400

    Slidetoggle wrote with pure javascript

--- app/js/navbar.js
+++ app/js/navbar.js
@@ -1,6 +1,30 @@
-$(document).ready(function () {
-    $( "#menu-btn" ).click(function() {
-        $(".sub-menu").slideToggle( "slow", function() {
+let menuBtn = document.getElementById("menu-btn");
+let subMenu = document.getElementsByClassName("sub-menu")[0];
+
+menuBtn.addEventListener("click", function(event){
+    //subMenu.classList.toggle("d-block");
+
+    event.preventDefault();
+
+    if (!subMenu.classList.contains('d-block')) {
+        subMenu.classList.add('d-block');
+        subMenu.style.height = 'auto';
+
+        let height = subMenu.clientHeight + "px";
+
+        subMenu.style.height = '0px';
+
+        setTimeout(function () {
+            subMenu.style.height = height;
+        }, 0);
+        } 
+        else {
+            subMenu.style.height = '0px';
+            subMenu.addEventListener('transitionend', function () {
+                subMenu.classList.remove('d-block');
+            }, 
+        {
+            once: true
         });
-    });
+    }
 });
